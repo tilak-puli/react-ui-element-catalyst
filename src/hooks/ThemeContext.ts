16007d62b5f180ba76c25149700a0d94ad10266f
Tilak Puli <tilakpuli15@gmail.com>
2023-10-17 18:23:15 +0530

    Add context for theme

---
+++ src/hooks/ThemeContext.ts
@@ -0,0 +1,6 @@
+import React from 'react';
+
+const ThemeContext = React.createContext('light');
+ThemeContext.displayName = 'Theme';
+
+export default ThemeContext;
--- src/index.tsx
+++ src/index.tsx
@@ -1,5 +1,11 @@
 import Element from './components/Element';
+import ThemeContext from './hooks/ThemeContext';
+import React from 'react';
 
 export * from './types';
 
-export const ElementComponent = Element;
+export const ElementComponent = (props) => (
+  <ThemeContext.Provider value={'light'}>
+    <Element {...props} />
+  </ThemeContext.Provider>
+);
