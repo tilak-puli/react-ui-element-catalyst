0ae9866da93a998779f3fa67003f515f6184df78
Tilak Puli <tilakpuli15@gmail.com>
2023-10-17 18:27:40 +0530

    Add styled components

--- src/components/Element.tsx
+++ src/components/Element.tsx
@@ -1,8 +1,13 @@
 import React from 'react';
 import {ElementProps} from '../types';
+import styled from 'styled-components';
 
 const Element = ({color, text}: ElementProps) => {
-  return <div style={{color}}>text is: {text}</div>;
+  return <StyledContainer color={color}>text is: {text}</StyledContainer>;
 };
+
+const StyledContainer = styled.div`
+  color: ${({color}) => color || 'red'};
+`;
 
 export default Element;
